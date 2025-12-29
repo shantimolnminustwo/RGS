c0abcaa6397edc96ebbc257be4e446e6980c3898
shantimolnminustwo <shantimol.nminustwo@gmail.com>
2025-12-29 15:40:59 +0530

    updates

--- rgs/vite.config.js
+++ rgs/vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-   base: '/rgs/',
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-  },
+  base: '/rgs/',
 })
