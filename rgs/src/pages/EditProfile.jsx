7dd25c828855a8f8b117359a4edb8fca3c6e66a6
shantimolnminustwo <shantimol.nminustwo@gmail.com>
2025-12-29 15:31:03 +0530

    fresh deployment setup

--- rgs/src/pages/EditProfile.jsx
+++ rgs/src/pages/EditProfile.jsx
@@ -19,7 +19,7 @@ const EditProfile = () => {
     }
   };
 
-  // ✉️ Email validation sss vvv
+  // ✉️ Email validation sss vvv 11
   const validateEmail = (value) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(value)) {
