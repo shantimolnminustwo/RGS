8c6971d427ca021d23175ebdd74edd5bc6088605
shantimolnminustwo <shantimol.nminustwo@gmail.com>
2025-12-29 16:30:17 +0530

    changes1

--- rgs/vite.config.js
+++ rgs/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/s
+// https://vite.dev/config/ss
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   base: '/rgs/',
