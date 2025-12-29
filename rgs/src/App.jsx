ee7cc6f068897d5bdce067ebea78885b2a529970
ShantyShaji <Shantees2000@gmail.com>
2025-12-29 10:57:36 +0530

    replaced browserrouter with hashrouter

--- rgs/src/App.jsx
+++ rgs/src/App.jsx
@@ -1,23 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+ import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Offers from "./pages/Offers";
 import OfferDetails from "./pages/OfferDetails";
-import Profile from "./pages/Profile.jsx";
+import Profile from "./pages/Profile";
 import EditProfile from "./pages/EditProfile";
-import RedeemedOffers from "./pages/RedeemedOffers.jsx";
+import RedeemedOffers from "./pages/RedeemedOffers";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/offers" element={<Offers />} />
         <Route path="/offers/:id" element={<OfferDetails />} />
-         <Route path="/profile" element={<Profile />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-          <Route path="/redeemed-offers" element={<RedeemedOffers />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/redeemed-offers" element={<RedeemedOffers />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
