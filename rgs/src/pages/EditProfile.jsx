dfcd6393aa98c23b362fd8b7a999ad0936f5cbcd
shantimolnminustwo <shantimol.nminustwo@gmail.com>
2025-12-29 16:43:32 +0530

    changes

--- rgs/src/pages/EditProfile.jsx
+++ rgs/src/pages/EditProfile.jsx
@@ -19,7 +19,7 @@ const EditProfile = () => {
     }
   };
 
-  // ✉️ Email validation sss vvv 114
+  // ✉️ Email validation
   const validateEmail = (value) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(value)) {
